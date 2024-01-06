aa5d8d0c7fde7eee0a2ca1d4450681d33b1e40b3
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-01-06 13:13:05 +0100

    completato esercizio somma e media - aggiunto file html per comodità

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,17 @@
   
   http://www.imparareaprogrammare.it
 */
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+let sum = a + b + c + d + e;
+
+let media = sum / 5;
+
+console.log(sum);
+
+console.log(media);
